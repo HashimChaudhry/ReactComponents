35a163aa91b3639813e954cb229e0e0edea300b0
HashimChaudhry <hashimc117@gmail.com>
2022-05-17 21:39:17 -0700

    Added in carousel module

---
+++ src/js/components/carousel/Card.js
@@ -0,0 +1,15 @@
+import react from "react"
+import styles from "../../css/card.module.css
+
+// A card component that has an image and title overlay
+// Can be flipped to provide a description if needed
+let ImageCard = ({ flip, text, image }) => {
+
+	return (
+		<div>
+								
+		</div>
+	)
+}
+
+export default OverlayTextCard;
