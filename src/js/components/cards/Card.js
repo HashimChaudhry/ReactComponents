9b777f7fa5b7765afbfc8e180546586d8619c6ae
Hashim Chaudhry <hashim0131@gmail.com>
2022-05-24 14:56:39 -0700

    Bug fix for incorrect animation speed in Card

--- src/js/components/cards/Card.js
+++ src/js/components/cards/Card.js
@@ -2,12 +2,10 @@ import React from "react";
 import styles from "./styles/card.module.css";
 import { useState, useEffect } from "react";
 import { CSSTransition } from "react-transition-group"; 
-import "./styles/transitions.css"
 
 let Card = ({ text, image, children }) => {
 	// State to manage hover animation
 	let [hover, setHover] = useState(false);
-	const hoverDuration = 300;
 	
 	// Function to manage hover state change
 	let toggleHover  = () => {
@@ -15,7 +13,12 @@ let Card = ({ text, image, children }) => {
 	}
 
 	return (
-		<CSSTransition in={hover} classNames="hoverGrow">
+		<CSSTransition in={hover} classNames={{
+				enterActive: styles.hoverGrowEnter,
+				enterDone: styles.hoverGrowEnterDone,
+				exitActive: styles.hoverGrowExit,
+				exitDone: styles.hoverGrowExitDone
+			}}>
 			<div onMouseLeave={toggleHover} onMouseEnter={toggleHover} className={styles.cardContainer}>
 				<span className={styles.image}>{children}</span>
 				<p>{text}</p>
