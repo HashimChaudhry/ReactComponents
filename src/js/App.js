c47874e91a97c7e2123249c496d5ccc5552d61b8
Hashim Chaudhry <hashim0131@gmail.com>
2022-05-25 12:56:01 -0700

    Fixed card props

--- src/js/App.js
+++ src/js/App.js
@@ -7,8 +7,8 @@ let App = () => {
 	let text = "Discover the future with our wide suite of products designed to help you out with the next phase of your company";
 	return (
 		<div className={styles.page}>
-			<Card width={"auto"} height={600} title={"Discover The Future"} text={text}>
-				<Logo />
+			<Card image={<Logo />} width={300} height={600}>
+				<p>Testing 1,2,3</p>	
 			</Card> 
 		</div>
 	)
--- src/js/components/cards/Card.js
+++ src/js/components/cards/Card.js
@@ -3,45 +3,16 @@ import styles from "./styles/card.module.css";
 import { useState, useEffect } from "react";
 import { CSSTransition } from "react-transition-group"; 
 
-let CardContainer = () => {
-
-}
-
-let Card = ({ text, title, children }) => {
-	// State to manage hover animation
-	let [hover, setHover] = useState(false);
-	
-	let animation = {
-		height: 1000 
-	}
-	
-	let toggleHover = () => {
-		setHover(!hover);
-	}
-
-	// Styles to add dynamically
-	let bodyStyle = generateFont();
-	let titleStyle = generateFont(25, 12);
-
+// Basic Card element with styles to set the width and height
+let Card = ({ image , children, width="auto", height="auto" }) => {
 	return (
-			<div className={styles.cardContainer}>
-				<span className={styles.image}>{children}</span>
+		<div style={{width: width, height: height}} className={styles.cardContainer}>
+				<span className={styles.image}>{image}</span>
 				<div className={styles.textContainer}>
-					{title}	
-					{text}
+					{children}
 				</div>
 			</div>	
 	)
-}
-
-
-let generateFont  = (fontSize=12, lineHeight=20) => {
-	let height = `${lineHeight}px`;
-
-	return ({
-		fontSize: fontSize,
-		lineHeight: height,
-	})
 }
 
 export default Card;
